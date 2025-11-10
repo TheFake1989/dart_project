21ea57510b56d3c2ed26aa09f81b8f03c1f4e58d
TheFake1989 <jonas.oerts.hansen@gmail.com>
2025-11-10 16:03:17 +0100

    added files, and moved files around

---
+++ models/rules.py
@@ -0,0 +1,11 @@
+#function import
+from models.player import *
+
+def around_the_world(players):
+    for player in players:
+        player.score = 1
+
+
+def shoot_down(players, shoot_down_target):
+    for player in players:
+        player.score = shoot_down_target
--- rules.py
+++
@@ -1,9 +0,0 @@
-#function import
-from models.player import *
-
-def around_the_world(player):
-    player.score = 0
-
-
-def shoot_down(player, shoot_down_target):
-    player.score = shoot_down_target
